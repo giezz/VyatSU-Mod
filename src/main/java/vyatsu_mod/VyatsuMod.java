47875ccba352a1588a0ed29b10dd08713a90b0a2
giezz <giezz@vk.com>
2022-08-06 03:41:37 +0300

    Rollback to 1.18.2 (done)

---
+++ src/main/java/vyatsu_mod/VyatsuMod.java
@@ -0,0 +1,27 @@
+package vyatsu_mod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(VyatsuMod.MOD_ID)
+public class VyatsuMod {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "vyatsu_mod";
+
+    public VyatsuMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
